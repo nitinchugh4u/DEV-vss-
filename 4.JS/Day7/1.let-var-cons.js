796b6b876a545cc2251f11d522938fd5b922d64b
Nitin chugh <nitinchugh4u@gamil.com>
2023-08-01 02:14:12 +0530

    pus

---
+++ 4.JS/Day7/1.let-var-cons.js
@@ -0,0 +1,21 @@
+// var
+
+var a =10;
+var a= 20;
+a = "hello";
+a= true;
+console.log(a);
+
+
+// let
+let a =10;
+// let a = 20;  //error
+a = "hello"
+console.log(a);
+
+
+
+// const 
+let a= 20;
+// a=30  error
+// let a ="hello"  error
