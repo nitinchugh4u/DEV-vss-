397e30200c49964ed23833e3ec2f3243540fae48
Nitin chugh <nitinchugh4u@gamil.com>
2023-08-08 20:47:27 +0530

    code

---
+++ 2-DSA/Day 24/1.move-0-1.js
@@ -0,0 +1,26 @@
+let arr = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0];
+// 0 to left
+// 1 to right
+
+//
+// 1.loop on array
+// 2.take first element and and check it is zero or non zero
+// and if it is nonzero then we have to compare that element with all the other element when we get the non zero then we have to swap both the element and and break that loop
+// 3.take first element and compare with all the other element
+//
+
+function find(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != 0) {
+      for (let j = i + 1; j < arr.length; i++) {
+        if (arr[i] !== arr[j]) {
+          let lastVar = arr[i];
+          arr[i] = arr[j];
+          arr[j] = lastVar;
+        }
+      }
+    }
+  }
+  return arr;
+}
+console.log(find(arr));
