4812531ceacaf17f57ef359ccad362d677f9082c
Nitin chugh <nitinchugh4u@gamil.com>
2023-08-12 18:51:08 +0530

    push

--- 2-DSA/Day 25/4.js
+++ 2-DSA/Day 25/4.js
@@ -1,5 +1,48 @@
-const arr = [1,2,3,4,5]
+let arr = [];
 console.log(arr);
-arr2 = arr.pop()
-console.log(arr2);
+arr.push(5);
 console.log(arr);
+arr.pop();
+console.log(arr);
+arr.push(1);
+arr.push(2);
+arr.push(3);
+arr.push(4);
+arr.push(5);
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.pop();
+console.log()
+arr.pop();
+console.log(arr);
+arr.pop();
+arr.pop()
+console.log(arr);
+arr.pop();
+console.log(arr);
+
+
+let str = "nitin"
+console.log(str);
+console.log(typeof str);
+
+const res = str.split("")
+console.log(res);
+res.pop()
+console.log(res);
+res.push(10);
+console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+
+
